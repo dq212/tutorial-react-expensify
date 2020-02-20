0a08dd569982f00cecac866079d78bd67eafdd6a
dq212 <>
2020-02-20 14:17:29 -0500

    ok

--- src/app.js
+++ src/app.js
@@ -18,7 +18,6 @@ const store = configureStore();
 store.subscribe(()=>{
     const state = store.getState();
     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
 })
 
 
